1b58dc2c0bb4266a1c6573992463ddbd15b12f97
Andrew Silluron <asilluron@users.noreply.github.com>
2019-04-29 18:41:25 -0700

    setup webpack

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# MWC Textfield for Alpha Dev
+While the material-components team is busy transforming material components into lit-element, they are not publishing certain elements that are not quite ready for production.
+
+This version has no warranty and is simply a good resource to install via npm until material-components team moves forward with publishing their version.
+
+## Development
+Under no circumstance, should the private flag be set to false or removed.
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './mwc-list-item.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'mwc-list-item.js'
+  },
+  optimization: {
+    sideEffects: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  performance: {
+    hints: false
+  }
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ])
+}
